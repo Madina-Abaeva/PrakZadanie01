ec40b518cfd4950ddf947bd530f7e6e651d3c6fc
Madina <ozdemirov24050@mail.ru>
2023-02-14 21:12:38 +0100

    Задача 4

--- Ex04/Program.cs
+++ Ex04/Program.cs
@@ -1,2 +1,33 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+int a = 4;
+int b = 8;
+int c= 12;
+int max;
+
+
+ if(a>b)  { 
+
+
+if(a>c) {  
+ max=a;
+Console.WriteLine(max);
+ }
+
+ }
+  else
+  {
+if(b>c)  { 
+ max=b;
+
+Console.WriteLine(max);
+
+ } 
+ else
+ {   
+ max=c;
+Console.WriteLine(max);  }
+
+ }
+
+
