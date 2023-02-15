ef864991ad90a1bbf3d5055d9b9a96ab38c05d83
Madina <ozdemirov24050@mail.ru>
2023-02-15 22:30:48 +0100

    practicheskoe zadanie 01

--- Ex06/Program.cs
+++ Ex06/Program.cs
@@ -1,8 +1,18 @@
 ﻿//Задача 6: Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+Console.WriteLine("Vvedite chislo x  ");
+int x = int.Parse(Console.ReadLine());
 
 
-Console.WriteLine("number"); 
+if(x%2==0) {
 
+
+Console.WriteLine("chislo chetnoe" +" "+  x ); 
 
 }
 
+else
+
+{
+
+   Console.WriteLine("chislo ne chetnoe"); 
+}
