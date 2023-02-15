4caae712936f5b59a2426e3c2d508df3ba92b2c7
Madina <ozdemirov24050@mail.ru>
2023-02-15 22:32:39 +0100

    practicheskoe zadanie 01

---
+++ Ex08/Program.cs
@@ -0,0 +1,17 @@
+﻿// 8. Показать четные числа от 1 до N
+Console.WriteLine("Vvedite n");
+int n = int.Parse(Console.ReadLine());
+int t= 1;
+
+while (t<n)
+{
+
+t=t+1;
+
+if(t%2==0)
+{
+ Console.WriteLine("четные числа" + " " + t);
+}
+
+}
+ 
