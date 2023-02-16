9d44a8c787405df3ef8b06a4f338c0b98092cb31
Madina <ozdemirov24050@mail.ru>
2023-02-16 18:42:52 +0100

    Ex13

---
+++ Ex13/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+int number3Value(int value)
+{
+    int number3 = value % 10;
+    return number3;
+}
+
+Console.WriteLine("Vvedite trexznachnoe chislo ");
+int value = int.Parse(Console.ReadLine());
+
+if(value < 100 )
+{
+    Console.WriteLine("третьей цифры нет ");
+}
+
+Console.WriteLine(number3Value(value));
+
