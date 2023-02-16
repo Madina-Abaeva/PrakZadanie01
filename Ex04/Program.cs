4a1c8526b3341f261ae0bd028b7cd386d255de33
Madina <ozdemirov24050@mail.ru>
2023-02-16 13:02:01 +0100

    Ex04

--- Ex04/Program.cs
+++ Ex04/Program.cs
@@ -1,33 +1,39 @@
 ﻿// Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+Console.WriteLine("Vvedite tri chisla");
+int a = int.Parse(Console.ReadLine());
+int b = int.Parse(Console.ReadLine());
+int c = int.Parse(Console.ReadLine());
 
-int a = 4;
-int b = 8;
-int c= 12;
 int max;
 
 
- if(a>b)  { 
+if (a > b)
+{
 
 
-if(a>c) {  
- max=a;
-Console.WriteLine(max);
- }
+    if (a > c)
+    {
+        max = a;
+        Console.WriteLine(max);
+    }
 
- }
-  else
-  {
-if(b>c)  { 
- max=b;
+}
+else
+{
+    if (b > c)
+    {
+        max = b;
 
-Console.WriteLine(max);
+       Console.WriteLine(max);
 
- } 
- else
- {   
- max=c;
-Console.WriteLine(max);  }
+    }
+    else
+    {
+        max = c;
+       Console.WriteLine(max);
+    }
 
- }
+    
+}
 
 
