12e34ab18332602cc22a2d5b2bd7d468316b7166
Madina <ozdemirov24050@mail.ru>
2023-02-17 18:46:02 +0100

    Ex10

---
+++ Ex10/Program.cs
@@ -0,0 +1,26 @@
+﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+
+
+
+int number2Value(value)
+{
+
+    int number2 = value / 10 % 10;
+    return number2;
+
+}
+
+Console.WriteLine(" Vvedite trexznachnoe chislo");
+int value = int.Parse(Console.ReadLine());
+
+
+if (value < 100 || value > 999)
+
+{
+
+    Console.WriteLine("vvedennoe chislo ne trexznachnoe");
+
+}
+
+  Console.WriteLine(number2Value(value));
