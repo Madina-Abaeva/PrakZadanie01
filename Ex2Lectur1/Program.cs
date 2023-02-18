334cb677dba20d1898e4c38121a567491d897ffe
Madina <ozdemirov24050@mail.ru>
2023-02-18 16:41:12 +0100

    Ex02Lectur1

---
+++ Ex2Lectur1/Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+int a = new Random().Next(1,10);
+Console.WriteLine(a);
+
+int b = new Random().Next(1,10);
+Console.WriteLine(b);
+
+int c = new Random().Next(1,10);
+Console.WriteLine(c);
+
+int d = new Random().Next(1,10);
+Console.WriteLine(d);
+int max = a ;
+if (a> max) max = a;
+if (b> max) max = b;
+if (c> max) max = c;
+if (d> max) max = d;
+Console.Write("max=");
+
+Console.WriteLine(max);
