79db2504156924fb34386eb88bf9e8a32413de34
Madina <ozdemirov24050@mail.ru>
2023-02-19 17:08:49 +0100

    Ex1Seminar3

---
+++ Ex1Seminar3/Program.cs
@@ -0,0 +1,20 @@
+﻿// pokazat' chetnoe chislo ot 1 do N
+void evenNumbers(int N)
+{
+    int index = 2;
+    while (index <= N)
+    {
+
+        System.Console.WriteLine(index);
+        index = index + 2;
+
+    }
+}
+int GetNumber(string text)
+{
+    System.Console.WriteLine(text);
+    return Convert.ToInt32(Console.ReadLine());
+
+}
+int N = GetNumber("vvedite chislo");
+evenNumbers(N);
