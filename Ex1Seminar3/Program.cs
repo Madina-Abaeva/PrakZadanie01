322436a82de0935ba392bbea0043a1cfed6b3295
Madina <ozdemirov24050@mail.ru>
2023-02-20 12:58:44 +0100

    Ex1Seminar3

--- Ex1Seminar3/Program.cs
+++ Ex1Seminar3/Program.cs
@@ -1,20 +1,22 @@
 ﻿// pokazat' chetnoe chislo ot 1 do N
-void evenNumbers(int N)
+void  evenNumbers(int N) 
 {
-    int index = 2;
-    while (index <= N)
-    {
+int index = 2;
+while(index <= N)
+{
 
-        System.Console.WriteLine(index);
-        index = index + 2;
+System.Console.WriteLine(index);
+index = index + 2;
 
-    }
 }
-int GetNumber(string text)
-{
-    System.Console.WriteLine(text);
-    return Convert.ToInt32(Console.ReadLine());
 
+}
+
+int GetNumber (string text)
+{
+System.Console.WriteLine(text);
+return Convert.ToInt32(Console.ReadLine());
+    
 }
 int N = GetNumber("vvedite chislo");
 evenNumbers(N);
