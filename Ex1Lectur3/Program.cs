c7b8aef846c128d60095fc30816b30788505eb4e
Madina <ozdemirov24050@mail.ru>
2023-02-21 12:21:46 +0100

    Ex1Lectur3

---
+++ Ex1Lectur3/Program.cs
@@ -0,0 +1,15 @@
+﻿
+//Method nichego ne vozvr,no prinimaet argum
+void Method2(string msg,int count)
+{
+int i = 0;
+while(i < count )
+{
+Console.WriteLine(msg);
+i++;
+
+}
+
+}
+
+Method2("tekst",4);
