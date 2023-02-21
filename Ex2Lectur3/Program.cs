0154a3ec0cb4d7646e1cd36d9bac964d5ac37ca8
Madina <ozdemirov24050@mail.ru>
2023-02-21 12:20:07 +0100

    Ex2Lectur3

---
+++ Ex2Lectur3/Program.cs
@@ -0,0 +1,14 @@
+﻿// prinimaet i vozvr 
+string Method(int count,string text)
+{
+int i = 0;
+string result = String.Empty;
+while(i<count)
+{ result= result + text;
+i++;
+}
+return result;
+}
+
+string res=Method(4,"text");
+Console.WriteLine(res);
