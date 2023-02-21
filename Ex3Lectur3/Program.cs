6b59c01139b7d913934e75ce6b80114284445318
Madina <ozdemirov24050@mail.ru>
2023-02-21 12:34:40 +0100

    Ex3Lectur3

---
+++ Ex3Lectur3/Program.cs
@@ -0,0 +1,15 @@
+﻿// for
+string Method(int count,string text)
+{
+string result=string.Empty;
+for(int i=0;i < count ; i++)
+{
+
+result = result + text;
+
+}
+
+return result;
+}
+string ex = Method(4,"text");
+Console.WriteLine(ex);
