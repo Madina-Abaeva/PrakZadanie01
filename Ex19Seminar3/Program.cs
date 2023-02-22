40a10808dc836efb9162756838a0d706482864ac
Madina <ozdemirov24050@mail.ru>
2023-02-22 11:20:56 +0100

    Ex19Seminar3

---
+++ Ex19Seminar3/Program.cs
@@ -0,0 +1,28 @@
+﻿// Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+bool Num(int N)
+{
+    int tempNumber = N;
+    int newNumber = 0;
+    while (N > 0)
+    {
+        int remNumber = N % 10;
+        N = N / 10;
+    newNumber = newNumber * 10 + remNumber;
+
+    }
+     return  tempNumber==newNumber;
+   
+
+
+}
+int Value(string text)
+{
+    Console.WriteLine(text);
+    return Convert.ToInt32(Console.ReadLine());
+}
+int number = Value("vvedite chislo");
+
+
+bool m = Num(number);
+Console.WriteLine(m);
+
