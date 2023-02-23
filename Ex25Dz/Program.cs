3af00fbc301da082beeaaaac8c35dfa5aea8fe00
Madina <ozdemirov24050@mail.ru>
2023-02-24 00:14:00 +0100

    Ex25DZ

---
+++ Ex25Dz/Program.cs
@@ -0,0 +1,39 @@
+﻿
+// Напишите метод, который принимает на вход два числа (A и B) и возводит число A в натуральную степень ,нельзя использовать Math.Pow
+
+
+
+int Main(int number1, int number2)
+
+{
+    int number = number1;
+    int ext = number2;
+    int stepen = 1;
+    int index = 1;
+    while (index <= ext)
+    {
+
+        stepen = stepen * number;
+        index++;
+       
+    }
+ Console.WriteLine(stepen);
+    return stepen;
+}
+
+int Value(string text)
+{
+    Console.WriteLine(text);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+
+
+
+int number1 = Value("vvedite chislo");
+int number2 = Value("vvedite stepen'");
+int P = Main(number1, number2);
+Console.WriteLine(P);
+
+
+
