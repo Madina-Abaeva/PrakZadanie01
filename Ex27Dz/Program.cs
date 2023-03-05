26ec9862ba4c07f7d957e3f65acb2bb36a1e5ae9
Madina <ozdemirov24050@mail.ru>
2023-03-05 13:48:35 +0100

    Ex27DZ

---
+++ Ex27Dz/Program.cs
@@ -0,0 +1,31 @@
+﻿// Задача 27: Напишите метод, который принимает на вход число и выдаёт сумму цифр в числе
+
+
+double Main(int N)
+    {
+ 
+        double summa = 0;
+        while (N / 10 >= 0)
+        {
+            summa = summa + N % 10;
+           Console.WriteLine(summa); 
+
+            N = N / 10;
+
+        }
+        
+        return summa;
+    }
+
+int PrinNumber(string text)
+{
+
+    Console.WriteLine(text);
+    return Convert.ToInt32(Console.ReadLine());
+
+
+}
+
+int N = PrinNumber("vvedite chislo");
+
+Main(N);
